157d2594bcad5610866677d9fb8c2759f02d6391
aelichung <aelichung@gmail.com>
2017-12-03 21:44:02 -0800

    add fixtures.js chckpt 31

---
+++ scripts/fixtures.js
@@ -0,0 +1,43 @@
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: '1881',
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs: [
+    {title: 'Blue', duration: '4:26'},
+    {title: 'Green', duration: '3:14'},
+    {title: 'Red', duration: '5:01'},
+    {title: 'Pink', duration: '3:21'},
+    {title: 'Magenta', duration: '2:15'}
+  ]
+};
+
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs: [
+    {title: 'Hello Operator?', duration: '1:01'},
+    {title: 'Ring, ring, ring', duration: '5:01'},
+    {title: 'Fits in your pocket', duration: '3:21'},
+    {title: 'Can you hear me now?', duration: '3:14'},
+    {title: 'Wrong phone number', duration: '2:15'}
+  ]
+};
+var albumBigFish = {
+  title: 'Big Fish',
+  artist: 'Jacques Cousteau',
+  label: 'Aqua-lung',
+  year: '1990',
+  albumArtUrl: 'assets/images/album_covers/12.png',
+  songs: [
+    {title: 'Great White Shark', duration: '1:01'},
+    {title: 'Baleen Whale', duration: '5:01'},
+    {title: 'Orca', duration: '3:21'},
+    {title: 'Tiger Shark', duration: '3:14'},
+    {title: 'Dolphin', duration: '2:15'}
+  ]
+};
